20c04539158f950def4576511143d4e18ec75865
turkandagdeviren <turkandagdeviren@yahoo.com>
2022-04-16 15:58:48 +0200

    added more hello feature

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,8 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Hello");
+        System.out.println("developing hello feature");
+        System.out.println("local repo is already published to GitHub");
+        System.out.println("doing more work");
     }
 }
--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -3,8 +3,6 @@ public class HelloTest {
     public static void main(String[] args) {
 
         System.out.println("Hello Test");
-        System.out.println("developing hello feature");
-        System.out.println("local repo is already published to GitHub");
-        System.out.println("doing more work");
+
     }
 }
